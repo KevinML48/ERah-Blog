1d492fd7fadd1fab5cc5f7765e77b28f0af7e56b
Kevin <kevinpromogreta@gmail.com>
2025-03-26 18:20:28 +0100

    Ajout de composants tests

---
+++ resources/js/Components/Hello.jsx
@@ -0,0 +1,12 @@
+// resources/js/Components/Hello.jsx
+
+const Hello = () => {
+    return (
+      <div className="text-center mt-8">
+        <h2 className="text-3xl font-semibold">Hello!</h2>
+      </div>
+    );
+  };
+  
+  export default Hello;
+  
